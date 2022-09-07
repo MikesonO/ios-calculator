50e170a71c7b67de1af6829572e4dfd00cf280fc
mikeson <mikeson.jnr@gmail.com>
2022-09-07 17:34:35 +0100

    Add operate function that calls operator functions

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ const multiply = (num1, num2) => num1 * num2;
 //Divide Funtion
 const divide = (num1, num2) => num1 / num2;
 
+const operate = (operator, num1, num2) => {
+  if (operator == add){
+    return add(num1, num2);
+  } else if (operator == subtract){
+    return subtract(num1, num2);
+  } else if (operator == multiply){
+    return multiply(num1, num2);
+  } else if (operator == divide){
+    return divide(num1, num2);
+  }
+}
 
