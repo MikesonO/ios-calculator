330fa0365e67d1f5bae29eea78a08ee6eb609334
mikeson <mikeson.jnr@gmail.com>
2022-09-07 17:18:52 +0100

    Add basic math operator functions

---
+++ index.js
@@ -0,0 +1,13 @@
+//Add Function
+const add = (num1,num2) => num1 + num2;
+
+//Subtract Function
+const subtract = (num1, num2) => num1 - num2;
+
+//Multiply Function
+const multiply = (num1, num2) => num1 * num2;
+
+//Divide Funtion
+const divide = (num1, num2) => num1 / num2;
+
+
