81d82e75a9565072d312705fb407f74b8f59b8be
mikeson <mikeson.jnr@gmail.com>
2022-09-08 22:00:25 +0100

    Beautified index.js file

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ let currentOperator = "";
 
 
 //Displays Number
-function displayNumber(num){
-  if(num==""){ //Checks calculator is empty
+function displayNumber(num) {
+  if (num == "") { //Checks calculator is empty
     display.innerText = num;
     controls[0].innerText = "AC";
-  } else{
-  controls[0].innerText = "C"; //Changes AC text to C
-  display.innerText = formatNumber(num);
+  } else {
+    controls[0].innerText = "C"; //Changes AC text to C
+    display.innerText = formatNumber(num);
   }
 }
 
 //Formats Number with commas
-function formatNumber(num){
+function formatNumber(num) {
   const MIN = 1;
   const MAX = 999999999;
-  let number = Math.min(Math.max(Number(num),MIN),MAX);
+  let number = Math.min(Math.max(Number(num), MIN), MAX);
   let formattedNumber = number.toLocaleString("en");
   return formattedNumber;
 }
@@ -33,33 +33,33 @@ userInput = () => {
   return display.innerText;
 }
 
-removeComma = (num) =>{
+removeComma = (num) => {
   return Number(num.replace(/,/g, ""));
 }
 
 //Number Buttons
-for(let i = 0; i < numbers.length; i++){
-  numbers[i].addEventListener("click", (event)=> {
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", (event) => {
     let input = removeComma(userInput());
-    if (input != NaN){//If output is a Number
+    if (input != NaN) { //If output is a Number
       input = input + event.target.id; //Concatenates pressed
       displayNumber(input);
-  }
-});
+    }
+  });
 }
 
 // Controls Buttons
-for(let i = 0; i < controls.length; i++){
-  controls[i].addEventListener("click", (event) =>{
-    if(event.target.id == "clear"){
+for (let i = 0; i < controls.length; i++) {
+  controls[i].addEventListener("click", (event) => {
+    if (event.target.id == "clear") {
       displayNumber("");
     }
 
-  }); 
+  });
 }
 
 //Add Function
-const add = (num1,num2) => num1 + num2;
+const add = (num1, num2) => num1 + num2;
 
 //Subtract Function
 const subtract = (num1, num2) => num1 - num2;
@@ -71,13 +71,13 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
 const operate = (operator, num1, num2) => {
-  if (operator == add){
+  if (operator == add) {
     return add(num1, num2);
-  } else if (operator == subtract){
+  } else if (operator == subtract) {
     return subtract(num1, num2);
-  } else if (operator == multiply){
+  } else if (operator == multiply) {
     return multiply(num1, num2);
-  } else if (operator == divide){
+  } else if (operator == divide) {
     return divide(num1, num2);
   }
 }
